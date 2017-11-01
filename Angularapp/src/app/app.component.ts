37edff8aaef4d3dff06a40095b533f64b3d9eb88
vivek.13462 <vivek.13462@gmail.com>
2017-10-31 17:03:28 -0700

    working with objects in component class and defining in inline html

--- Angularapp/src/app/app.component.ts
+++ Angularapp/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hi, This is inline template of html</h1>',
+  template: `<h1>Hi! This is inline template of html..</h1>
+  <p> {{UserObj.name}} </p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  UserObj = {
+  name: "Vivek",
+  location: "California"
+  }
 }
