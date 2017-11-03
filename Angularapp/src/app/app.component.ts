b69f016dd4b36b997a7e5e427b2d1a70553292c3
vivek.13462 <vivek.13462@gmail.com>
2017-11-02 19:55:57 -0700

    event binding using click event

--- Angularapp/src/app/app.component.ts
+++ Angularapp/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hi! This is property binding in  Angular4..</h1>
- <img src="{{someimage}}">
- <img [src]="someimage">
- <img bind-src="someimage">`,
+  template: `
+  <button (click)="firstFunction($event)"> Click ME </button>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-someimage = 'https://angular.io/resources/images/logos/angular2/angular.png';
+firstFunction(event){
+console.log(event);
+alert("HELLO");
+}
 }
