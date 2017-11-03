82df61fe4b20d59ff587b9458a2117def9d99a03
vivek.13462 <vivek.13462@gmail.com>
2017-11-02 19:59:19 -0700

    event binding using mouseenter event

--- Angularapp/src/app/app.component.ts
+++ Angularapp/src/app/app.component.ts
@@ -3,12 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <button (click)="firstFunction($event)"> Click ME </button>`,
+  <button (click)="firstFunction($event)"> Click ME </button>
+  <button (mouseenter)="secondFunction($event)"> Click ME </button>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 firstFunction(event){
 console.log(event);
 alert("HELLO");
+};
+secondFunction(event){
+console.log(event);
+alert("HELLO AGAIN");
 }
 }
